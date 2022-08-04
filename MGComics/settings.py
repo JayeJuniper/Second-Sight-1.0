2f0e1ca3c8c819d267d6979264690b82a5a66ea9
JonathanGrenz <jonathan.grenz@gmail.com>
2022-08-04 14:35:58 -0700

    Update settings.py

--- MGComics/settings.py
+++ MGComics/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['second-sight.herokuapp.com']
 
 
 # Application definition
